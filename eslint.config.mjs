e5037cdd8aef453f7edc7a01a73893a9a5ccdf07
markos3d <markost3d@gmail.com>
2024-10-05 18:11:50 +0200

    eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,163 @@
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+
+// export default [
+//   {files: ["**/*.{js,mjs,cjs,ts}"]},
+//   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+// ];
+
+// @ts-check
+
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+// For a detailed explanation, visit: https://github.com/angular-eslint/angular-eslint/blob/main/docs/CONFIGURING_FLAT_CONFIG.md
+import angular from 'angular-eslint';
+import ngrx from '@ngrx/eslint-plugin/v9';
+
+export default tseslint.config(
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['src/**/*.ts'],
+    extends: [...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylistic, ...angular.configs.tsRecommended],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.spec.json'],
+      },
+    },
+    processor: {
+      // https://github.com/angular-eslint/angular-eslint/issues/1917
+      meta: {
+        name: 'extract-inline-html',
+      },
+      ...angular.processInlineTemplates,
+    },
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/relative-url-prefix': 'error',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/member-ordering': [
+        'error',
+        {
+          default: [
+            'public-static-field',
+            'protected-static-field',
+            'private-static-field',
+            'public-instance-field',
+            'protected-instance-field',
+            'private-instance-field',
+            'constructor',
+            'public-static-method',
+            'protected-static-method',
+            'private-static-method',
+            'public-instance-method',
+            'protected-instance-method',
+            'private-instance-method',
+          ],
+        },
+      ],
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-extraneous-class': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/no-unnecessary-condition': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
+      '@typescript-eslint/unbound-method': 'off',
+      'arrow-body-style': 'error',
+      curly: 'error',
+      eqeqeq: ['error', 'always', { null: 'ignore' }],
+      'guard-for-in': 'error',
+      'no-bitwise': 'error',
+      'no-caller': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-eval': 'error',
+      'no-labels': 'error',
+      'no-new': 'error',
+      'no-new-wrappers': 'error',
+      'object-shorthand': ['error', 'always', { avoidExplicitReturnArrows: true }],
+      radix: 'error',
+      'spaced-comment': ['warn', 'always'],
+    },
+  },
+  {
+    files: ['src/main/webapp/**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-function': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+  {
+    // Html templates requires some work
+    ignores: ['**/*.html'],
+    extends: [prettier],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      // 👇 Use all rules at once
+      ...ngrx.configs.all,
+      // 👇 Or only import the rules for a specific package
+      ...ngrx.configs.store,
+      ...ngrx.configs.effects,
+      ...ngrx.configs.componentStore,
+      ...ngrx.configs.operators,
+      ...ngrx.configs.signals,
+    ],
+    rules: {
+      // 👇 Configure specific rules
+      '@ngrx/with-state-no-arrays-at-root-level': 'warn',
+    },
+  },
+  prettier,
+);
